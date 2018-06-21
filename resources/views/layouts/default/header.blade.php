115870900773b3a17658e3fa8bf3c5d64e512b83
Gihovani Filipp Pereira Demétrio <gihovani@gmail.com>
2018-06-21 18:11:58 -0300

    Alterado o link do titulo

--- resources/views/layouts/default/header.blade.php
+++ resources/views/layouts/default/header.blade.php
@@ -6,7 +6,7 @@
     <nav>
         <div class="nav-wrapper">
             <div class="container">
-                <a href="" class="brand-logo">
+                <a href="/" class="brand-logo">
                     {{ __('My Heroes - Forum') }}
                 </a>
 
